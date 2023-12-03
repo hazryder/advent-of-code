8530d8fe81ceb098e922f84ef554d74aed4ce54e
Harry Ryder <hazryder@gmail.com>
2023-12-03 14:34:29 +0000

    Day 3 second star

--- 2023/day/3/index.js
+++ 2023/day/3/index.js
@@ -13,7 +13,27 @@ function calculateFirstStar() {
     let validNumbers = calculateValidNumbers(cells, validCells)
 
     let starValue = validNumbers.reduce((acc, curr) => {
-        return acc + curr
+        return acc + curr.value
+    }, 0)
+
+    console.log("Star value:", starValue)
+}
+
+function calculateSecondStar() {
+    let grid = loadInputFromFile("./input.txt")
+
+    let cells = createCellArray(grid)
+
+    let validCells = calculateCellsWithAdjacentSpecialChars(cells)
+
+    let validNumbers = calculateValidNumbers(cells, validCells)
+
+    let starCells = calculateStarCells(cells)
+
+    let validStarCells = calculateValidStarCells(validNumbers, starCells)
+
+    let starValue = validStarCells.reduce((acc, curr) => {
+        return acc + curr.gearRatio
     }, 0)
 
     console.log("Star value:", starValue)
@@ -36,13 +56,13 @@ function calculateCellsWithAdjacentSpecialChars(cells) {
             let cell = cells[y][x]
 
             if (cellContainsCharacter(cell, NUMBERS_MATCH)) {
-                if(cellHasAdjacentCharacter(cells, cell, SPECIAL_CHARS_MATCH)) {
+                if (cellHasAdjacentCharacter(cells, cell, SPECIAL_CHARS_MATCH)) {
                     let cellValid = true
 
                     // Seach right
                     for (let x = cell.x + 1; true; x++) {
                         let lateralCell = cells[cell.y][x]
-            
+
                         if (lateralCell && cellContainsCharacter(lateralCell, NUMBERS_MATCH)) {
                             if (validCells.includes(lateralCell)) {
                                 cellValid = false
@@ -56,7 +76,7 @@ function calculateCellsWithAdjacentSpecialChars(cells) {
                     // Search left
                     for (let x = cell.x - 1; true; x--) {
                         let lateralCell = cells[cell.y][x]
-            
+
                         if (lateralCell && cellContainsCharacter(lateralCell, NUMBERS_MATCH)) {
                             if (validCells.includes(lateralCell)) {
                                 cellValid = false
@@ -67,7 +87,7 @@ function calculateCellsWithAdjacentSpecialChars(cells) {
                         }
                     }
 
-                    if(cellValid) {
+                    if (cellValid) {
                         validCells.push(cell)
                     }
                 }
@@ -78,11 +98,26 @@ function calculateCellsWithAdjacentSpecialChars(cells) {
     return validCells
 }
 
+function calculateStarCells(cells) {
+    let starCells = []
+
+    for (let y = 0; y < cells.length; y++) {
+        for (let x = 0; x < cells[0].length; x++) {
+            if(cells[y][x].value === "*") {
+                starCells.push(cells[y][x])
+            }
+        }
+    }
+
+    return starCells
+}
+
 function calculateValidNumbers(cells, validCells) {
     let validNumbers = []
 
     for (let cell of validCells) {
-        let number = `${cell.value}`
+        let _value = `${cell.value}`
+        let _cells = [cell]
 
         // Check each following lateral char for numbers, until one isn't found
         // Concat all these values
@@ -90,7 +125,9 @@ function calculateValidNumbers(cells, validCells) {
             let adjacentCell = cells[cell.y][x]
 
             if (adjacentCell && cellContainsCharacter(adjacentCell, NUMBERS_MATCH)) {
-                number = `${number}${adjacentCell.value}`
+                _value = `${_value}${adjacentCell.value}`
+
+                _cells.push(adjacentCell)
             }
             else {
                 break
@@ -103,17 +140,51 @@ function calculateValidNumbers(cells, validCells) {
             let adjacentCell = cells[cell.y][x]
 
             if (adjacentCell && cellContainsCharacter(adjacentCell, NUMBERS_MATCH)) {
-                number = `${adjacentCell.value}${number}`
+                _value = `${adjacentCell.value}${_value}`
+
+                _cells = [adjacentCell].concat(_cells)
             }
             else {
                 break
             }
         }
 
-        validNumbers.push(Number(number))
+        validNumbers.push({ value: Number(_value), cells: _cells })
     }
 
     return validNumbers
+}
+
+function calculateValidStarCells(validNumbers, starCells) {
+    // for each * cell, check each surrounding cell
+    // if two surrounding cells are contained within validnumbercells, but not within the same number, add to valid gears
+    let validStarCells = []
+
+    for (let starCell of starCells) {
+        let adjacentValidNumbers = []
+
+        for (let y = starCell.y - 1; y <= (starCell.y + 1); y++) {
+            for (let x = starCell.x - 1; x <= (starCell.x + 1); x++) {
+                for (let number of validNumbers) {
+                    for (let numberCell of number.cells) {
+                        if(numberCell.x === x && numberCell.y === y && !adjacentValidNumbers.includes(number)) {
+                            adjacentValidNumbers.push(number)
+                        }
+                    }
+                }
+            }
+        }
+
+        if(adjacentValidNumbers.length === 2) {
+            validStarCells.push({
+                cell: starCell,
+                gearRatio: Number(adjacentValidNumbers[0].value) * Number(adjacentValidNumbers[1].value),
+                adjacentNumbers: adjacentValidNumbers
+            })
+        }
+    }
+
+    return validStarCells
 }
 
 function cellContainsCharacter(cell, match) {
@@ -142,3 +213,4 @@ function cellHasAdjacentCharacter(cells, cell, match) {
 }
 
 calculateFirstStar()
+calculateSecondStar()
